5aca12548a4303b900ed439ec978e58bf5b41835
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-03-23 16:13:41 -0700

    added title div

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>AWS S3 Standard vs S3 Infrequent Access</h1>
+        <p>Cost Calculator (All units per month)</p>
+      </div>
+      <div className="divider"></div>
+      <div className="chart"></div>
     </div>
   );
 }
