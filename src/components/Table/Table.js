7e4dbbd15d64ac07a1edb620505cb45fd614877b
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-03-23 21:00:24 -0700

    modified table values

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,48 +1,53 @@
 import React from 'react';
 import './Table.scss';
 import { Table } from 'react-bootstrap';
+import Unit from '../Unit/Unit';
 
 export default function PriceList() {
+
+  const changePrice = (value) => {
+    console.log(value)
+  }
   return (
-    <Table id="table" responsive="sm">
+    <Table id="table" responsive="sm" bordered>
       <thead>
         <tr>
-          <th>#</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
+          <th>Resource</th>
+          <th>Unit</th>
+          <th>S3 Standard</th>
+          <th>S3 Infrequent Access</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>1</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
+          <td id="title">Data Size in TB</td>
+          <td><Unit unitName={"TB"} onChange={changePrice} /></td>
+          <td>$ 0.0</td>
+          <td>$ 0.0</td>
         </tr>
         <tr>
-          <td>2</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
+          <td id="title">No. of POST/PUT requests in Millions</td>
+          <td><Unit unitName={"Million"} onChange={changePrice} /></td>
+          <td>$ 0.0</td>
+          <td>$ 0.0</td>
         </tr>
         <tr>
-          <td>3</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
+          <td id="title">No. of GET requests in Millions</td>
+          <td><Unit unitName={"Million"} onChange={changePrice} /></td>
+          <td>$ 0.0</td>
+          <td>$ 0.0</td>
+        </tr>
+        <tr>
+          <td id="title">S3 IA Data transfer cost in GB</td>
+          <td><Unit unitName={"GB"} onChange={changePrice} /></td>
+          <td>$ 0.0</td>
+          <td>$ 0.0</td>
+        </tr>
+        <tr id="total-row">
+          <td id="title">Totals</td>
+          <td>Savings: $ 0.0</td>
+          <td>$ 0.0</td>
+          <td>$ 0.0</td>
         </tr>
       </tbody>
     </Table>
---
+++ src/components/Unit/Unit.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { InputGroup, FormControl } from 'react-bootstrap';
+
+export default function Unit(props) {
+  const handleOnChange = (value) => {
+    props.onChange(value);
+  }
+  return (
+    <InputGroup
+      className="mb-3"
+      onChange={e => handleOnChange(e.target.value)}>
+      <InputGroup.Prepend>
+      </InputGroup.Prepend>
+      <FormControl />
+      <InputGroup.Append>
+        <InputGroup.Text>{props.unitName}</InputGroup.Text>
+      </InputGroup.Append>
+    </InputGroup>
+  )
+}
+
